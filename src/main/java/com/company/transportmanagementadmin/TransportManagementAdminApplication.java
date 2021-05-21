645f4321212e271e0a5d8468faa570c52427950c
Deepak Suresh S <deepaksuresh10@gmail.com>
2021-05-21 19:27:33 +0530

    Initial commit

---
+++ src/main/java/com/company/transportmanagementadmin/TransportManagementAdminApplication.java
@@ -0,0 +1,13 @@
+package com.company.transportmanagementadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportManagementAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportManagementAdminApplication.class, args);
+	}
+
+}
